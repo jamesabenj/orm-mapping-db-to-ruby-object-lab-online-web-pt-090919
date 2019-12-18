5d8349954e742939ab6b653e050e88b6449523a2
James Benjamin <james.abenj@gmail.com>
2019-12-18 02:33:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,7 +83,7 @@ class Student
       end
     end
 
-    def self.first_X_students_in_grade_10(X)
+    def self.first_X_students_in_grade_10(x)
       sql = <<-SQL
       SELECT name
       FROM students
