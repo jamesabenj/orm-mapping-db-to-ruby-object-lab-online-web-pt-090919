f12569185443d1c4b77a3789af0e029bc166b623
James Benjamin <james.abenj@gmail.com>
2019-12-18 03:03:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -106,5 +106,15 @@ class Student
       end.first
     end
 
+    def self.all_students_in_grade_X(x)
+      sql = <<-SQL
+      SELECT name
+      FROM students
+      WHERE grade = ?
+        SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+      end
+
 
 end
